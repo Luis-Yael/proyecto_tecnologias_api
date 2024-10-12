d923ba8b8a57ebbb1e11b87b96d9e7b9a8ef7f69
Luis Yael Mendez <luisyaelms@gmail.com>
2024-10-12 13:26:27 -0600

    Initial commit

---
+++ proyecto_tecnologias_api/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for proyecto_tecnologias_api project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_tecnologias_api.settings')
+
+application = get_wsgi_application()
